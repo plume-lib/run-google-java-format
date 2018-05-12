c5a3c31888749ff47a7412d9830614c784b01009
Michael Ernst <mernst@cs.washington.edu>
2018-05-12 14:50:05 -0700

    Fix typo

--- run-google-java-format.py
+++ run-google-java-format.py
@@ -26,8 +26,8 @@ fixup_py = os.path.join(script_dir, "fixup-google-java-format.py")
 ## To use an officially released version.
 # Version 1.3 and earlier do not wrap line comments.
 gjf_version = os.getenv("GJF_VERSION", "1.5")
-gjf_snapshot = os.getenv("GJF_VERSION", "")
-gjf_url_base = os.getenv("GJF_VERSION", "https://github.com/google/google-java-format/releases/download/google-java-format-" + gjf_version + "/")
+gjf_snapshot = os.getenv("GJF_SNAPSHOT", "")
+gjf_url_base = os.getenv("GJF_URL_BASE", "https://github.com/google/google-java-format/releases/download/google-java-format-" + gjf_version + "/")
 ## To use a non-official version by default, because an official version is
 ## unusably buggy (like 1.1) or no new release has been made in a long time.
 ## Never change the file at a URL; make it unique by adding a date.
