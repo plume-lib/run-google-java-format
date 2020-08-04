d887b08bb2dd86808f12b75035236b894a373db9
Michael Ernst <mernst@cs.washington.edu>
2020-08-03 17:07:42 -0700

    Use spaces instead of tabs

--- run-google-java-format.el
+++ run-google-java-format.el
@@ -9,34 +9,34 @@
 if it matches a hard-coded list of directories."
   (interactive)
   (let ((cmd
-	 (cond
-	  ((or (and (string-match-p "/\\(randoop\\)" (buffer-file-name))
-		    (not (string-match-p "CloneVisitor\\.java$" (buffer-file-name))))
-	       (and (string-match-p "/daikon" (buffer-file-name))
-		    (not (string-match-p "\\.jpp$" (buffer-file-name))))
-	       (and (string-match-p "/toradocu" (buffer-file-name))
-		    (not (string-match-p "/src/test/resources/" (buffer-file-name))))
-	       (and (string-match-p "/plume-lib" (buffer-file-name))
-		    (not (string-match-p "WeakHasherMap.java$\\|WeakIdentityHashMap.java$"
-					 (buffer-file-name))))
-	       (string-match-p "/org/plumelib/" (buffer-file-name)))
-	   ;; normal formatting
-	   "run-google-java-format.py ")
-	  ((and (string-match-p "/checker-framework" (buffer-file-name))
-		(not (string-match-p "/checker-framework-inference" (buffer-file-name)))
-		(not (string-match-p "/checker/jdk/" (buffer-file-name)))
-		(not (string-match-p "\.astub$" (buffer-file-name)))
-		)
-	   ;; non-standard cammand-line arguments
-	   "run-google-java-format.py -a ")
-	  (t
-	   ;; for all other projects, don't automatically reformat
-	   nil))))
+         (cond
+          ((or (and (string-match-p "/\\(randoop\\)" (buffer-file-name))
+                    (not (string-match-p "CloneVisitor\\.java$" (buffer-file-name))))
+               (and (string-match-p "/daikon" (buffer-file-name))
+                    (not (string-match-p "\\.jpp$" (buffer-file-name))))
+               (and (string-match-p "/toradocu" (buffer-file-name))
+                    (not (string-match-p "/src/test/resources/" (buffer-file-name))))
+               (and (string-match-p "/plume-lib" (buffer-file-name))
+                    (not (string-match-p "WeakHasherMap.java$\\|WeakIdentityHashMap.java$"
+                                         (buffer-file-name))))
+               (string-match-p "/org/plumelib/" (buffer-file-name)))
+           ;; normal formatting
+           "run-google-java-format.py ")
+          ((and (string-match-p "/checker-framework" (buffer-file-name))
+                (not (string-match-p "/checker-framework-inference" (buffer-file-name)))
+                (not (string-match-p "/checker/jdk/" (buffer-file-name)))
+                (not (string-match-p "\\.astub$" (buffer-file-name)))
+                )
+           ;; non-standard cammand-line arguments
+           "run-google-java-format.py -a ")
+          (t
+           ;; for all other projects, don't automatically reformat
+           nil))))
     (if cmd
-	(progn
-	  ;; I would like to avoid the "(Shell command succeeded with no output)"
-	  ;; message.
-	  (shell-command (concat cmd (buffer-file-name)) "*run-google-java-format*")
-	  ;; If you use bdiff, use this version instead
-	  ;; (bdiff-revert-buffer-maybe)
-	  (revert-buffer nil t)))))
+        (progn
+          ;; I would like to avoid the "(Shell command succeeded with no output)"
+          ;; message.
+          (shell-command (concat cmd (buffer-file-name)) "*run-google-java-format*")
+          ;; If you use bdiff, use this version instead
+          ;; (bdiff-revert-buffer-maybe)
+          (revert-buffer nil t)))))
