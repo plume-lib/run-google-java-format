9b8f6a97d5b4eb6ea151d429d88b3c4e3ef57734
Michael Ernst <mernst@cs.washington.edu>
2022-07-13 10:32:39 -0700

    Use GJF version 1.15.0

--- run-google-java-format.py
+++ run-google-java-format.py
@@ -43,9 +43,12 @@ java_version = re.search('\"(\d+(\.\d+)?).*\"', java_version_string).groups()[0]
 ## To use an officially released version.
 ## (Releases appear at https://github.com/google/google-java-format/releases/.)
 # Version 1.3 and earlier do not wrap line comments.
-# Version 1.8 and later require JDK 11 to run and reflow string literals.
+# Version 1.8 and later require JDK 11 to run, and it reflows string literals.
+# Note that due to changes since GJF 1.7, formatting with GJF 1.7 is
+# inconsistent with later versions of GJF, so you might want to just disable
+# formatting on Java 8 if you also use a later version of Java.
 # Version 1.10.0 and later can run under JDK 16.
-gjf_version_default = "1.7" if (java_version == "1.8") else "1.13.0"
+gjf_version_default = "1.7" if (java_version == "1.8") else "1.15.0"
 gjf_version = os.getenv("GJF_VERSION", gjf_version_default)
 gjf_download_prefix = "v" if re.match(r'^1\.1[0-9]', gjf_version) else "google-java-format-"
 gjf_snapshot = os.getenv("GJF_SNAPSHOT", "")
@@ -141,6 +144,8 @@ if (java_version == "1.8"):
     jdk_opens = []
 else:
     # From https://github.com/google/google-java-format/releases/
+    # This is no longer required as of GJF version 1.15.0, but users might
+    # supply a version number lower than that.
     jdk_opens = [
       "--add-exports", "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
       "--add-exports", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
